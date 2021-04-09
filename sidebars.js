19a94340abc1dc3200311639dd8f3bc53e8aa9d6
SAY-droid427 <sayanimallick0218@gmail.com>
2021-04-09 12:23:27 +0530

    Added in sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -39,7 +39,8 @@ module.exports = {
       label: 'Data Structures',
       items: [
         'data-structures/data-structures',
-        'data-structures/binary-tree'
+        'data-structures/binary-tree',
+        'data-structures/palindromic-linked-list'
       ],
     },
     {
