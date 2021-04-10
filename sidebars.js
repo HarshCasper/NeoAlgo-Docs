924afc2c5ccf02b90a48bd9fcb82ac0013777de5
Amit kumar mishra <akm43300@gmail.com>
2021-04-10 16:44:15 +0530

    Update

--- sidebars.js
+++ sidebars.js
@@ -1,158 +1,5 @@
 module.exports = {
 
-<<<<<<< HEAD
-  docs: [
-    {
-      type: 'category',
-      label: 'Introduction',
-      items: [
-        'introduction/About-NeoAlgo',
-        'introduction/development-setup',
-        'introduction/contributor-handbook',
-      ]
-    },
-    {
-      type: 'category',
-      label: 'Code Playgrounds',
-      items: [
-        'code-playgrounds/python-playground',
-        'code-playgrounds/java-playground',
-      ]
-    },
-    {
-      type: 'category',
-      label: 'Searching Algorithms',
-      items: [
-        'searching-algorithms/searching-algorithms',
-        'searching-algorithms/linear-search',
-        'searching-algorithms/interpolation-search'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Sorting Algorithms',
-      items: [
-        'sorting-algorithms/sorting-algorithms',
-        'sorting-algorithms/bubble-sort'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Data Structures',
-      items: [
-        'data-structures/data-structures',
-        'data-structures/binary-tree'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Dynamic Programming',
-      items: [
-        'dynamic-programming/dynamic-programming',
-        'dynamic-programming/egg-dropping-problem'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Computational Geometry',
-      items: [
-        'computational-geometry/computational-geometry',
-        'computational-geometry/jarvis-algorithm'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Mathematics',
-      items: [
-        'mathematics/mathematics',
-        'mathematics/armstrong-number',
-	      'mathematics/factorial-of-number',
-        'mathematics/prime-number',
-        'mathematics/factorial-of-number',
-	      'mathematics/happy-number',
-        'mathematics/palindrome-number'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Bit Manipulation',
-      items: [
-        'bit-manipulation/bit-manipulation',
-        'bit-manipulation/brian-kernighan-algorithm'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Graph Algorithms',
-      items: [
-        'graph-algorithms/graph-algorithms',
-        'graph-algorithms/breadth-first-search'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Competitive Programming',
-      items: [
-        'competitive-programming/competitive-programming',
-        'competitive-programming/anagram-numbers'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Machine Learning',
-      items: [
-        'machine-learning/machine-learning',
-        'machine-learning/linear-regression'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Cryptography',
-      items: [
-        'cryptography/cryptography',
-        'cryptography/caesar-cipher',
-	'cryptography/vigenere-cipher'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Backtracking',
-      items: [
-        'backtracking/backtracking',
-        'backtracking/n-queens'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Operating Systems',
-      items: [
-        'operating-systems/operating-systems',
-        'operating-systems/banker-algorithm'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Miscellaneous',
-      items: [
-        'miscellaneous/miscellaneous',
-        'miscellaneous/sudoku-solver'
-      ],
-    },
-	  {
-      type: 'category',
-      label: 'Docusaurus Tutorial',
-      items: [
-        'getting-started',
-        'create-a-page',
-        'create-a-document',
-        'create-a-blog-post',
-        'markdown-features',
-        'thank-you',
-      ],
-    },
-  ],
-};
-=======
     docs: [
         {
             type: "category",
@@ -221,8 +68,9 @@ module.exports = {
                 "mathematics/factorial-of-number",
                 "mathematics/magic-number",
                 "mathematics/happy-number",
+                "mathematics/palindrome-number",
                 "mathematics/krishnamurthy-number",
-		"mathematics/neon-number",
+		            "mathematics/neon-number"
             ],
         },
         {
@@ -297,4 +145,3 @@ module.exports = {
         },
     ],
 };
->>>>>>> 47f76f4831a92749e3328decd71728b171b136ce
