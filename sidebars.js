93896b69e5854df986b74302f73df294b15e4789
tushar khandelwal <tusharkhandelwal315@gmail.com>
2021-04-14 13:28:50 +0530

    kadane algorithm

--- sidebars.js
+++ sidebars.js
@@ -54,6 +54,7 @@ module.exports = {
             items: [
                 "dynamic-programming/dynamic-programming",
                 "dynamic-programming/egg-dropping-problem",
+                "dynamic-programming/kadane-algorithm",
             ],
         },
         {
