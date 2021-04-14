f6cad8480f633f8f7ef3c4a81e9a1112f1901d82
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-04-14 17:35:30 +0530

    Catalan number (#229)
    * Added doc for catalan number
    
    * Updated sidebar.js
    
    * Updated
    
    * Added some styles
    
    Co-authored-by: Harsh Bardhan Mishra <erbeusgriffincasper@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -85,6 +85,7 @@ module.exports = {
                 "mathematics/sum-of-divisors",
                 "mathematics/count-of-divisors",
                 "mathematics/count-of-digits",
+		"mathematics/catalan-number",
                 "mathematics/reverse-a-number",
             ],
         },
