a303e4dda4ddfc88574a2d3c83f0391d10c142be
nidh-ish <nidhishbhimrajka1@gmail.com>
2021-04-16 00:07:45 -0400

    Added documentation for Burrows-Wheeler Transform

--- sidebars.js
+++ sidebars.js
@@ -128,7 +128,7 @@ module.exports = {
         {
             type: "category",
             label: "Cryptography",
-            items: ["cryptography/cryptography", "cryptography/caesar-cipher", "cryptography/keyword-cipher", "cryptography/playfair-cipher", "cryptography/railfence-cipher", "cryptography/vernam-cipher", "cryptography/homophonic-cipher"],
+            items: ["cryptography/cryptography", "cryptography/caesar-cipher", "cryptography/keyword-cipher", "cryptography/playfair-cipher", "cryptography/railfence-cipher", "cryptography/vernam-cipher", "cryptography/homophonic-cipher", "cryptography/burrows-wheeler-transform"],
         },
         {
             type: "category",
