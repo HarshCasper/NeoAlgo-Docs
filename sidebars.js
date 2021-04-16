27a54cef665560360f5a4f43028ea8baeedb926d
Nidhish Bhimrajka <54209827+nidh-ish@users.noreply.github.com>
2021-04-16 09:47:04 +0530

    Added documentation for Hill Cipher (#260)

--- sidebars.js
+++ sidebars.js
@@ -130,7 +130,7 @@ module.exports = {
         {
             type: "category",
             label: "Cryptography",
-            items: ["cryptography/cryptography", "cryptography/caesar-cipher", "cryptography/keyword-cipher", "cryptography/playfair-cipher", "cryptography/railfence-cipher", "cryptography/vernam-cipher", "cryptography/homophonic-cipher"],
+            items: ["cryptography/cryptography", "cryptography/caesar-cipher", "cryptography/keyword-cipher", "cryptography/playfair-cipher", "cryptography/railfence-cipher", "cryptography/vernam-cipher", "cryptography/homophonic-cipher", "cryptography/hill-cipher"],
         },
         {
             type: "category",
