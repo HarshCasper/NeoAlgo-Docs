76f842986b0d565dd008e773f9a55ee9a6151cef
Shivam Mahajan <76818369+shivammahajancse@users.noreply.github.com>
2021-04-17 22:31:52 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -49,11 +49,7 @@ module.exports = {
                 "data-structures/data-structures",
                 "data-structures/binary-tree",
                 "data-structures/Singly-Linked-list",
-                "data-structures/binary-search-tree",            
-                "data-structures/preorder_traversal",
-                "data-structures/inorder_traversal",
-                "data-structures/Circular-Linked-list",              
-                "data-structures/preorder_traversal"
+                "data-structures/inorder_traversal",                
 
             ],
         },
