9729d8446352da997b5577c7eca97fc3745e6544
Adithya kurien Peter <adithya.kurien@gmail.com>
2021-04-20 11:19:45 +0530

    Matrix Addition (#164)
    * adding matrix addition
    
    * changes
    
    * changes
    
    * Update matrix-addition.mdx
    
    * Update matrix-addition.mdx
    
    * Update matrix-addition.mdx
    
    Co-authored-by: Harsh Mishra <erbeusgriffincasper@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -112,6 +112,7 @@ module.exports = {
 		            "mathematics/hamming-distance",
                 "mathematics/triangular-numbers",
 		            "mathematics/lucas-theorem",
+                "mathematics/matrix-addition"
             ],
         },
         {
