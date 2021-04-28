a8c411d440bed8f6b11cb160649f4842ff806a3c
TUSHAR KHANDELWAL <56471793+tk315@users.noreply.github.com>
2021-04-28 12:57:43 +0530

    Rotate Matrix by 90 Degree (#454)

--- sidebars.js
+++ sidebars.js
@@ -131,6 +131,7 @@ module.exports = {
                 "mathematics/hexagonal-number",
                 "mathematics/spiral-traversal-of-matrix",
                 "mathematics/tetrahedral-number",
+                "mathematics/rotate-matrix-by-90-degree",
             ],
         },
         {
