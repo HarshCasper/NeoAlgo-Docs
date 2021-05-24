d07f03e791cdf9e543317947f4499338c922d16d
Saatvik Nagpal <saatvikngpl21@gmail.com>
2021-05-24 17:05:52 +0530

    Added documentation for right view of BT

--- sidebars.js
+++ sidebars.js
@@ -204,6 +204,7 @@ module.exports = {
                 "competitive-programming/generate-valid-parantheses",
                 "competitive-programming/left-view-of-BT",
                 "competitive-programming/top-view-of-BT",
+                "competitive-programming/right-view-of-BT",
 
             ],
         },
