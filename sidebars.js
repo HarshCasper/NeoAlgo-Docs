a9126b1a0d026b92426008ab50d72027a314a911
SAY-droid427 <sayanimallick0218@gmail.com>
2021-05-30 18:39:23 +0530

    Added doc for activity selection

--- sidebars.js
+++ sidebars.js
@@ -169,6 +169,7 @@ module.exports = {
                 "competitive-programming/max-number-from-array",
                 "competitive-programming/reverse-of-string",
                 "competitive-programming/diameter-of-BT",
+                "competitive-programming/activity-selection",
             ],
         },
         {
